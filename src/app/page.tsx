8d6d929053f1e5eb6c596e3640a9697d33faa651
24LP2437 24LP2437 <24LP2437@G304-CC0232.local>
2025-04-24 21:37:25 +0800

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,28 @@ import { Header } from "./_components/Header";
 
 export default function Home() {
   return (
-    <div className="w-[59.688rem] h-[61.625rem] flex flex-col gap-5 ">
+    <div>
       <Header />
-      <div className="w-[95%] h-64 border rounded-lg p-6 flex flex-col gap-3 "></div>
-      <div>
-        <Label className="font-semibold text-base  ">Recent transactions</Label>
-      </div>
-      <div className="w-[95%] max-h-[41.5rem] border rounded-lg p-6 flex flex-col justify-center items-center gap-3 ">
-        <div className="w-96 h-32 flex flex-col gap-5 items-center justify-center ">
-          <div>
-            <Heart className="w-8 h-7" />
-          </div>
-          <div>
-            <h6 className="font-semibold text-base text-center ">
-              You don’t have any supporters yet
-            </h6>
-            <p className="font-normal text-base text-center">
-              Share your page with your audience to get started.
-            </p>
+      <div className="w-[59.688rem] h-[61.625rem] flex flex-col gap-5 ">
+        <div className="w-[95%] h-64 border rounded-lg p-6 flex flex-col gap-3 "></div>
+        <div>
+          <Label className="font-semibold text-base  ">
+            Recent transactions
+          </Label>
+        </div>
+        <div className="w-[95%] max-h-[41.5rem] border rounded-lg p-6 flex flex-col justify-center items-center gap-3 ">
+          <div className="w-96 h-32 flex flex-col gap-5 items-center justify-center ">
+            <div>
+              <Heart className="w-8 h-7" />
+            </div>
+            <div>
+              <h6 className="font-semibold text-base text-center ">
+                You don’t have any supporters yet
+              </h6>
+              <p className="font-normal text-base text-center">
+                Share your page with your audience to get started.
+              </p>
+            </div>
           </div>
         </div>
       </div>
